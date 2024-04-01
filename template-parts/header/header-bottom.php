22ee2acb87b41b82dff0f970a2d3d6058a761e25
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-02 00:18:49 +0800

    Update header-bottom.php

--- template-parts/header/header-bottom.php
+++ template-parts/header/header-bottom.php
@@ -26,20 +26,20 @@ $header_icon_menu = $Theme_Options->header_icon_menu;
 					</a>
 				</li>
 				<li class="col-lg-auto col-md-4 col-6">
-					<a href="" class="d-flex align-items-center justify-content-center text-white flex-column flex-lg-row h-100">
+					<a href="https://nationalwarmth.co.uk/services/air-source-heat-pumps/" class="d-flex align-items-center justify-content-center text-white flex-column flex-lg-row h-100">
 						<span class="icon"><img src="/wp-content/uploads/2022/07/heat-pumps-1.png" alt=""></span>
 						<span class="text">Heat Pumps</span>
 					</a>
 				</li>
 				<li class="col-lg-auto col-md-4 col-6">
-					<a href="" class="d-flex align-items-center justify-content-center text-white flex-column flex-lg-row h-100">
+					<a href="https://nationalwarmth.co.uk/services/solar-power/" class="d-flex align-items-center justify-content-center text-white flex-column flex-lg-row h-100">
 
 						<span class="icon"><img src="/wp-content/uploads/2022/07/solar.png" alt=""></span>
 						<span class="text">Solar PV</span>
 					</a>
 				</li>
 				<li class="col-lg-auto col-md-4 col-6">
-					<a href="" class="d-flex align-items-center justify-content-center text-white flex-column flex-lg-row h-100">
+					<a href="https://nationalwarmth.co.uk/services/eco4-boiler-upgrade-grant/" class="d-flex align-items-center justify-content-center text-white flex-column flex-lg-row h-100">
 						<span class="icon"><img src="/wp-content/uploads/2022/07/eco.png" alt=""></span>
 						<span class="text">ECO4 / BUS</span>
 					</a>
