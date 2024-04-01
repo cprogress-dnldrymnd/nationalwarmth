c49602e7b0383388376237edcd22f69f70832c93
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-01 22:01:54 +0800

    Update page-form.php

--- templates/page-form.php
+++ templates/page-form.php
@@ -14,9 +14,22 @@ $icon = get__post_meta('icon');
     <section class="form-section md-padding">
         <div class="container">
             <div class="inner text-center">
-				<?= do_shortcode('[contact-form-7 id="4ab7e41" title="Find installer Form"]') ?>
+                <?= do_shortcode('[contact-form-7 id="4ab7e41" title="Find installer Form"]') ?>
             </div>
         </div>
     </section>
 </main>
 <?php get_footer('form'); ?>
+
+<script>
+    jQuery(document).ready(function() {
+        $key = 1;
+        $length = jQuery('.fieldset-cf7mls').length() + 1;
+        $per_step = 100 / $length;
+        console.log($per_step);
+        jQuery('.fieldset-cf7mls').each(function(index, element) {
+            // element == this
+
+        });
+    });
+</script>
