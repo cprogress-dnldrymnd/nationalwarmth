7b77b47e01f26e6514bfd1a0d24c6658cee80cf9
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-01 23:19:34 +0800

    Update post-types.php

--- includes/post-types.php
+++ includes/post-types.php
@@ -166,16 +166,6 @@ new newPostType(
 );
 
 
-new newPostType(
-	array(
-		'name'          => 'Services',
-		'singular_name' => 'Service',
-		'icon'          => 'dashicons-camera-alt',
-		'rewrite'       => array('slug' => 'services'),
-		'supports'      => array('title', 'revisions', 'editor', 'thumbnail'),
-		'show_in_rest'  => true,
-	)
-);
 
 
 
