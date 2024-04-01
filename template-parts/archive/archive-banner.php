ec164998a868b3e4ea7427cb19bb308658483ef2
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-02 00:16:49 +0800

    Update archive-banner.php

--- template-parts/archive/archive-banner.php
+++ template-parts/archive/archive-banner.php
@@ -18,7 +18,7 @@ if (is_single()) {
 	}
 }
 ?>
-<section class="project-title-section archive-banner md-padding" data-aos="fade-left">
+<section class="project-title-section archive-banner md-padding">
 	<div class="container">
 		<h1 class="mb-4"><?= $title ?></h1>
 		<?php
