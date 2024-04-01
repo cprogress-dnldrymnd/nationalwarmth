07bfc125381da75b0fe634eb4a57f61021f41924
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-02 00:31:40 +0800

    Update single.php

--- single.php
+++ single.php
@@ -21,7 +21,7 @@ get_header(); // This fxn gets the header.php file and renders it
 			if (get_post_type() != 'landingpages'&& get_post_type() != 'post' && get_post_type() != 'partners') {
 				get_template_part('template-parts/section/content', 'banner');
 			} 
-			if(get_post_type() == 'post' || get_post_type() != 'partners') {
+			if(get_post_type() == 'post' || get_post_type() == 'partners') {
 				get_template_part('template-parts/archive/archive', 'banner');
 			}
 		}
