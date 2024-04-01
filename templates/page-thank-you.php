2e1880defd8de5fe5cbc1a0f267fb640a6cca5ca
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-01 22:15:21 +0800

    Update page-thank-you.php

--- templates/page-thank-you.php
+++ templates/page-thank-you.php
@@ -8,17 +8,29 @@
 $icon = get__post_meta('icon');
 ?>
 <main id="main" class="page-components">
+
+    <section class="image-section lg-padding thank-you">
+        <div class="container text-center">
+            <img src="<?= wp_get_attachment_image_url($icon, 'medium') ?>" />
+            <div class="heading-box mb-4 mt-4">
+                <h2> <?= get__post_meta('alt_title') ? get__post_meta('alt_title') : get_the_title() ?> </h2>
+            </div>
+            <div class="subscribe-box">
+
+                <div class="form-box">
+                    <?= do_shortcode('[contact-form-7 id="85" title="Subscribe Form"]') ?>
+                </div>
+            </div>
+        </div>
+    </section>
     <section class="thank-you md-padding">
         <div class="container">
             <div class="inner text-center">
-				<img src="<?= wp_get_attachment_image_url($icon, 'medium') ?>" />
-                <div class="heading-box mb-4 mt-4">
-                    <h2> <?= get__post_meta('alt_title') ? get__post_meta('alt_title') : get_the_title() ?> </h2>
-                </div>
+
                 <div class="description-box">
                     <?php the_content() ?>
                 </div>
-                <div class="button-box  button-accent">
+                <div class="button-box  button-accent button-bordered">
                     <a href="<?= get_site_url() ?>">
                         <span class="text">
                             Return to home
