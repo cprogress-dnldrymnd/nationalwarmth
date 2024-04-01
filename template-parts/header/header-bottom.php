6155daf263db7d6b6abd45853ccbd86039a64e7b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-01 20:46:31 +0800

    Update header-bottom.php

--- template-parts/header/header-bottom.php
+++ template-parts/header/header-bottom.php
@@ -5,7 +5,7 @@ $header_icon_menu = $Theme_Options->header_icon_menu;
 if ($header_icon_menu) {
 ?>
 
-	<section class="header-bottom header-icon-list small-text">
+	<section class="header-bottom header-icon-list small-text d-none">
 		<div class="header-bottom-holder background-secondary">
 			<div class="container-fluid">
 				<ul class="d-flex justify-content-center align-items-center list-inline row mb-0 fw-light g-0 flex-wrap flex-lg-nowrap">
@@ -24,3 +24,49 @@ if ($header_icon_menu) {
 	</section>
 
 <?php } ?>
+
+<section class="header-bottom header-icon-list small-text">
+	<div class="header-bottom-holder background-secondary">
+		<div class="container-fluid">
+			<ul class="d-flex justify-content-center align-items-center list-inline row mb-0 fw-light g-0 flex-wrap flex-lg-nowrap">
+				<li class="col-lg-auto col-md-4 col-6">
+					<a href="" class="d-flex align-items-center justify-content-center text-white flex-column flex-lg-row h-100">
+						<span class="icon"><img src="/wp-content/uploads/2022/07/boilers.png" alt=""></span>
+						<span class="text">Boilers</span>
+					</a>
+				</li>
+				<li class="col-lg-auto col-md-4 col-6">
+					<a href="" class="d-flex align-items-center justify-content-center text-white flex-column flex-lg-row h-100">
+						<span class="icon"><img src="/wp-content/uploads/2022/07/heating-systems.png" alt=""></span>
+						<span class="text">Heating Systems</span>
+					</a>
+				</li>
+				<li class="col-lg-auto col-md-4 col-6">
+					<a href="" class="d-flex align-items-center justify-content-center text-white flex-column flex-lg-row h-100">
+						<span class="icon"><img src="/wp-content/uploads/2022/07/insulation.png" alt=""></span>
+						<span class="text">Insulation</span>
+					</a>
+				</li>
+				<li class="col-lg-auto col-md-4 col-6">
+					<a href="" class="d-flex align-items-center justify-content-center text-white flex-column flex-lg-row h-100">
+						<span class="icon"><img src="/wp-content/uploads/2022/07/heat-pumps-1.png" alt=""></span>
+						<span class="text">Heat Pumps</span>
+					</a>
+				</li>
+				<li class="col-lg-auto col-md-4 col-6">
+					<a href="" class="d-flex align-items-center justify-content-center text-white flex-column flex-lg-row h-100">
+
+						<span class="icon"><img src="/wp-content/uploads/2022/07/solar.png" alt=""></span>
+						<span class="text">Solar PV</span>
+					</a>
+				</li>
+				<li class="col-lg-auto col-md-4 col-6">
+					<a href="" class="d-flex align-items-center justify-content-center text-white flex-column flex-lg-row h-100">
+						<span class="icon"><img src="/wp-content/uploads/2022/07/eco.png" alt=""></span>
+						<span class="text">ECO4 / BUS</span>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</section>
