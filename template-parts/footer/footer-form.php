9c481f8da8e4435627d0cfe43a115a5f76a15b36
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-07 17:30:27 +0800

    Update footer-form.php

--- template-parts/footer/footer-form.php
+++ template-parts/footer/footer-form.php
@@ -20,7 +20,36 @@
 </div>
 
 <script>
-    var myOffcanvas = document.getElementById('footerForm')
-    var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas)
-    bsOffcanvas.show();
+    jQuery(document).ready(function() {
+        if (!getCookie('energy_matters_display')) {
+            var myOffcanvas = document.getElementById('footerForm')
+            var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas)
+            bsOffcanvas.show();
+            setCookie('energy_matters_display', 'false');
+        }
+    });
+
+
+
+    function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    function getCookie(cname) {
+        let name = cname + "=";
+        let ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
 </script>
