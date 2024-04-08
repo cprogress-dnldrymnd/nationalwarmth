44d489c34e2fbed9ff065847ea3f7beefb2cfc55
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-09 04:40:32 +0800

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -9,25 +9,14 @@ jQuery(document).ready(function ($) {
 });
 
 function anchor_scroll() {
-	jQuery("a").on('click', function (event) {
-		// Make sure this.hash has a value before overriding default behavior
-		if (this.hash !== "") {
-			// Prevent default anchor click behavior
-			event.preventDefault();
+	var $root = $('html, body');
 
-			// Store hash
-			var hash = this.hash;
+	jQuery('a[href^="#"]').click(function () {
+		$root.animate({
+			scrollTop: jQuery(jQuery.attr(this, 'href')).offset().top - 80
+		}, 800);
 
-			// Using jQuery's animate() method to add smooth page scroll
-			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-			jQuery('html, body').animate({
-				scrollTop: jQuery(hash).offset().top - 80
-			}, 800, function () {
-
-				// Add hash (#) to URL when done scrolling (default click behavior)
-				window.location.hash = hash;
-			});
-		} // End if
+		return false;
 	});
 }
 
