d1127de9b1892a1977de9c586b4b2cf264e58cbc
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-11 14:27:58 +0800

    Update header-navigation.php

--- template-parts/header/header-navigation.php
+++ template-parts/header/header-navigation.php
@@ -25,23 +25,35 @@ $SVG = new SVG();
 								'walker' => new bootstrap_5_wp_nav_menu_walker()
 							));
 							?>
-						</nav>
-					</div>
+
+							<?php
+							wp_nav_menu(array(
+								'theme_location' => 'mobile-menu',
+								'container' => false,
+								'menu_class' => 'd-none d-lg-flex',
+								'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+								'items_wrap' => '<ul id="%1$s" class="navbar-nav align-items-center fw-light mt-5 mt-lg-0 %2$s">%3$s</ul>',
+								'depth' => 2,
+								'walker' => new bootstrap_5_wp_nav_menu_walker()
+							));
+							?>
+					</nav>
+				</div>
 
 
-					<div class="d-block d-lg-none text-end">
-						<button class="navbar-toggler toggler-menu" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
-							<span class="navbar-toggler-icon"> 
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-							</span>
-						</button>
-					</div>
+				<div class="d-block d-lg-none text-end">
+					<button class="navbar-toggler toggler-menu" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</span>
+					</button>
 				</div>
 			</div>
-			
-			
 		</div>
+
+
+	</div>
 	</div>
 </section>
